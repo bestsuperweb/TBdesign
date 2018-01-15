72c66db2c8fb49b4d26645988dc8c61c4092534e
bestsuperweb <bestsuperweb@gmail.com>
2018-01-14 22:46:43 -0500

    update homepage...

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,4 +2,30 @@ $(document).ready(function(){
     
     new WOW().init();
 
+    smoothScroll.init();
+
+    $('.flexslider1').flexslider({
+        animation: "slide",
+        controlNav: "thumbnails"
+    });
+
+	$('.flexslider1 .flex-control-nav li').each(function(index, element){
+		setTimeout( function(){
+				    $(element).addClass('animated slideOutDown');
+				  }, (100 * index));
+	});
+
+	$(window).scroll(function(){
+		if($('#testimonial').length){
+			var top = $('#testimonial').offset().top - 600;
+		    if($(this).scrollTop() >= top){	        
+				$('.flexslider1 .flex-control-nav li').each(function(index, element){
+					setTimeout( function(){
+							    $(element).removeClass('slideOutDown').addClass('animated bounceInUp');
+							  }, (100 * index));
+				});
+		    }
+		}	    
+	});
+
 });
